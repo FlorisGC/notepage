3e362d81c72a1fa5ca8ee0b3736a8a4558809002
Halleriet <floris.halleriet@student.graafschapcollege.nl>
2023-08-31 10:11:26 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+
+
+
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness:Brightness.dark,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        textTheme: const TextTheme(
+        ),
+      ),
+      home: const NotePage(),
+    );
+  }
+}
+
+class NotePage extends StatefulWidget {
+  const NotePage({Key? key}) : super(key: key);
+
+  @override
+  NotePageState createState() => NotePageState();
+}
+
+
+
+
+
+class NotePageState extends State<NotePage> {
+  TextEditingController noteTextEditingController = TextEditingController();
+  List<String> enteredTexts = [];
+
+  void addText(String newText) {
+    setState(() {
+      if (newText.length >= 5)
+      {
+      enteredTexts.insert(0, newText);
+
+      noteTextEditingController.clear();
+      }
+    });
+  }
+
+  void removeText(int index) {
+    setState(() {
+      enteredTexts.removeAt(index);
+    });
+  }
+
+  void markText(int index) {
+    setState(() {
+      if (enteredTexts[index].endsWith("    ✓")) {
+        enteredTexts[index] = enteredTexts[index].substring(0, enteredTexts[index].length - 5);
+      } else {
+        enteredTexts[index] = "${enteredTexts[index]}    ✓";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView( padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SafeArea(
+              child: TextField(
+                controller: noteTextEditingController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Type a note, min 5 characters",
+                ),
+                maxLength: 999,
+                onSubmitted: (text) {
+                  addText(noteTextEditingController.text);
+                },
+              ),
+            ),
+            for (var i = 0; i < enteredTexts.length; i++)
+            GestureDetector(
+              onLongPress: () {
+                removeText(i);
+              },
+              onDoubleTap: () {
+                markText(i);
+              },
+              child: Padding(padding: const EdgeInsets.all(10),
+              child: Text(enteredTexts[i],
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              ),
+            ),
+          ],
+        ),
+      ),
+     ),
+    );
+  }
+}
